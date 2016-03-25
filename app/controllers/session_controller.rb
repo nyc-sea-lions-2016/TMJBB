36b6ad9a27603ca7aa9f2138c9843d665a254821
Brett Ripley & Mike Gwozdek <pair+drumguy16.gwoz@devbootcamp.com>
2016-03-25 18:51:44 -0400

    Add login/logout

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,28 @@
 enable :sessions
 
+post '/users/create' do
+  @user = User.create(params[:user])
+  session[:id] = @user.id
+  redirect "/users/show/#{@user.id}"
+end
+
 get '/users/login' do
   @user = User.new
   erb :'/users/login'
 end
 
 get '/users' do
+  user = User.new(params[:user])
   @user = User.find_by(username: params[:user][:username])
-  redirect "/users/show/#{@user.id}"
+  if @user && @user.authenticate(params[:user][:password])
+    session[:id] = @user.id
+    redirect "/users/show/#{@user.id}"
+  else
+    erb :'/users/login'
+  end
+end
+
+get '/users/logout' do
+  session.clear
+  redirect '/users/login'
 end

a6499020b5a0fdb91df375752947d8b9f806e2e0
Brett Ripley & Mike Gwozdek <pair+drumguy16.gwoz@devbootcamp.com>
2016-03-25 20:14:47 -0400

    Route refactor. Fixed views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,15 @@
 get '/' do
+  @user = User.new
   erb :'/users/new'
 end
 
+# NEW
 get '/users/new' do
   @user = User.new
   erb :'/users/new'
 end
 
+# SHOW
 get '/users/show/:id' do
   @user = User.find(params[:id])
   erb :'/users/show'
